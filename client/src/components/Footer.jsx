5d676e0fdb3ac1fbf6adcf259c7eb7c4089f941c
Julia Kim <juliakimseohyeon@gmail.com>
2024-07-20 20:19:26 -0700

    Add different languages to Footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -12,17 +12,23 @@ Can replace by other Tailwind UI components
 
 const Footer = () => (
     <footer className="flex flex-row w-full bg-green p-6 justify-between fixed bottom-0">
-        <Link to="/" className="flex flex-col items-center">
+        <Link to="/" className="flex flex-col items-center w-1/3">
             <img src={SearchIcon} alt="Search Icon" />
-            <p className="text-lightgreen">Search</p>
+            <p className="text-lightgreen">
+                <FormattedMessage id="footer.search" />
+            </p>
         </Link>
-        <Link to="/recipes" className="flex flex-col items-center">
+        <Link to="/recipes" className="flex flex-col items-center w-1/3">
             <img src={RecipesIcon} alt="Recipes Icon" />
-            <p className="text-lightgreen">My Recipes</p>
+            <p className="text-lightgreen">
+                <FormattedMessage id="footer.myRecipes" />
+            </p>
         </Link>
-        <Link to="/settings" className="flex flex-col items-center">
+        <Link to="/settings" className="flex flex-col items-center w-1/3">
             <img src={SettingsIcon} alt="Settings Icon" />
-            <p className="text-lightgreen">Settings</p>
+            <p className="text-lightgreen">
+                <FormattedMessage id="footer.settings" />
+            </p>
         </Link>
     </footer>
 )
