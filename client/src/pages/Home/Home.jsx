14eda8aa00cfdfbbbe82f9404d54994ec7952a38
Julia Kim <juliakimseohyeon@gmail.com>
2024-07-23 12:58:33 -0700

    Add multilingual versions of recipe name in recipe data

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -9,7 +9,7 @@ export default function Home() {
             imageSrc:
                 'https://images.unsplash.com/photo-1670398564097-0762e1b30b3a?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
             href: 'www.fakeurl.com',
-            name: 'Delicious Food Name',
+            name: <FormattedMessage id="recipes.name" />,
         },
         {
             id: 2,
@@ -17,7 +17,7 @@ export default function Home() {
             imageSrc:
                 'https://images.unsplash.com/photo-1670398564097-0762e1b30b3a?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
             href: 'www.fakeurl.com',
-            name: 'Delicious Food Name',
+            name: <FormattedMessage id="recipes.name" />,
         },
     ]
 
