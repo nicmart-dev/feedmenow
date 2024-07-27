d6c8740acb7d28ec61733b1fcd575d8c9da16e3a
yaacovkdev <yaacov.kochatkov@gmail.com>
2024-07-26 17:54:39 -0700

    deleted gemini integration in express

--- server/routes/geminiRecipesRoutes.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const geminiRecipesControllerecipesController = require("../controllers/geminiRecipesController");
-
-router.route("/").get(geminiRecipesControllerecipesController.getAllRecipes);
-
-module.exports = router;
